d1bba33db6e6509ca7dbd2775bf5818f4cf6f9b5
eehgotz <eehgotz@risd.edu>
2021-02-03 19:54:53 -0500

    six update again again

--- lvgrm.js
+++ lvgrm.js
@@ -1,3 +1,3 @@
 $(document).ready(function() {
-  alert("Use the arrow keys to move and the mouse to look around. To continue exploring, go inside the next room cube.");
+  alert("Use the arrow keys to move and the mouse to look around. To continue exploring, click on the next room cube.");
     })
