b4e3c3ad555e0597b0fdee696cc382662775d6c6
eehgotz <eehgotz@risd.edu>
2022-01-30 12:22:43 -0500

    sunday funday

---
+++ thesisws_appendix.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+  $("#neuromancer").hover(
+    function(){
+      $("#neuromancerImg").show();
+  },
+    function () {
+      $("#neuromancerImg").hide();
+     }
+  );
+
+  $("#sistine").hover(
+    function(){
+      $("#sistineImg").show();
+  },
+    function () {
+      $("#sistineImg").hide();
+     }
+  );
+
+  $("#alice").hover(
+    function(){
+      $("#aliceImg").show();
+  },
+    function () {
+      $("#aliceImg").hide();
+     }
+  );
+
+  $("#blade").hover(
+    function(){
+      $("#bladeImg").show();
+  },
+    function () {
+      $("#bladeImg").hide();
+     }
+  );
+
+  $("#planet").hover(
+    function(){
+      $("#planetImg").show();
+  },
+    function () {
+      $("#planetImg").hide();
+     }
+  );
+
+})
