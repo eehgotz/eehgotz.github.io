cca9304c2df3a0e20cf0875b7d30689f684c9b7f
eehgotz <eehgotz@risd.edu>
2022-05-13 17:07:03 -0400

    may 13 update

--- thesis_night.js
+++ thesis_night.js
@@ -10,40 +10,79 @@ $(document).ready(function() {
      }
   );
 
+  $("#yakuzaPop1").click(function(){
+  $("#yakuzaPop2").show();
+  });
+  $("#yakuzaPop1").click(function(){
+  $("#yakuzaPop1").hide();
+  });
+
+  $("#yakuzaPop2").click(function(){
+  $("#yakuzaPop2").hide();
+  });
+
+
+  function geishaPop() {
+    $("#geishaPop1").show();
+    }
+  setTimeout(geishaPop, 30000);
+
+  $("#geishaPop1").click(function(){
+  $("#geishaPop1").hide();
+  });
+
+
+  function samuraiPop() {
+    $("#samuraiPop1").show();
+    }
+  setTimeout(samuraiPop, 60000);
+
+  $("#samuraiPop1").click(function(){
+  $("#samuraiPop2").show();
+  });
+  $("#samuraiPop1").click(function(){
+  $("#samuraiPop1").hide();
+  });
+
+  $("#samuraiPop2").click(function(){
+  $("#samuraiPop2").hide();
+  });
 
   function popUp1() {
     $("#pop1").show();
     }
-  setTimeout(popUp1, 12000);
+  setTimeout(popUp1, 20000);
 
   function popUp2() {
     $("#pop2").show();
     }
-  setTimeout(popUp2, 18000);
+  setTimeout(popUp2, 35000);
 
   function popUp3() {
     $("#pop3").show();
     }
-  setTimeout(popUp3, 23000);
+  setTimeout(popUp3, 50000);
 
   function popUp4() {
     $("#pop4").show();
     }
-  setTimeout(popUp4, 28000);
+  setTimeout(popUp4, 65000);
 
   function popUp5() {
     $("#pop5").show();
     }
-  setTimeout(popUp5, 33000);
+  setTimeout(popUp5, 75000);
 
   function popUp6() {
     $("#pop6").show();
     }
-  setTimeout(popUp6, 48000);
+  setTimeout(popUp6, 80000);
 
   function popUp7() {
     $("#pop7").show();
     }
-  setTimeout(popUp7, 53000);
+  setTimeout(popUp7, 85000);
+
+
 
 })
